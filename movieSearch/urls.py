5efef12ed24ec7aa633151050bce89a0ed95abaf
Talantone <talgatsitd@gmail.com>
2023-08-28 10:44:13 +0200

    added requirements.txt

--- movieSearch/urls.py
+++ movieSearch/urls.py
@@ -6,7 +6,7 @@ from movieSearch.views import home, search, ActorDetailView, MovieDetailView
 urlpatterns = [
     path('', lambda req: redirect('/home')),
     path('home', home, name='home'),
-    path('results', search, name="search"),
+    path('results/', search, name="search"),
     path('actor-details/<int:pk>', ActorDetailView.as_view(), name='actor-details'),
     path('movie-details/<int:pk>', MovieDetailView.as_view(), name='movie-details')
 ]
